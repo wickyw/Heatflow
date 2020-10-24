b2ac84196b60f738d665762849fc2d9862f25689
Wick Wijnholds <w.j.wijnholds@student.utwente.nl>
2020-10-24 20:03:00 +0200

    Ik kom er niet uit, maar toch maar een commit

--- exchangermodel.m
+++ exchangermodel.m
@@ -16,36 +16,41 @@ N = 100; % number of model segments per plate
 dl = l/N; % length of model segment
 dt = 5; % time step, s
 tmax = length(parallelTemp)*dt; % end time, s
-v = 5; % Fluid flow rate, m/s
+v = 1; % Fluid flow rate, m/s
 sigma = dl/v; % time constant
 cflow = dt/sigma; % constant for convective heat transport
-alpha = 10; % Convection coefficient
+alpha = 25; % Convection coefficient
 lambda = 400;
-Rth = (dp/lambda+2/alpha)*1/A/N;
+Rth = (dp/lambda+2/alpha)/A*N;
 rho = 1e3; % Density of water, kg/m³
-mWaterSegment = dl*w*df*rho;
+mWaterSegment = V*rho;
 cp = 4.2e3; % Water cp, J/kg/K
-Tin = zeros(length(parallelTemp),2*np-1);
-for i = 1:2:np-1
-    Tin(:,i) = parallelTemp(:,3);
-end
-for i = 2:2:np-1
-    Tin(:,i) = parallelTemp(:,5);
-end
+
 %% Model data arrays initialisation:
 Tmodel = zeros(tmax/dt,2*np-1,N);
-dTflow = zeros(2*np-1,N);
-dTplate = zeros(2*np-1,N); % Temperature change through interaction through plate
+Tin = zeros(length(parallelTemp),2*np-1);
+for i = 1:2:2*np-1
+    Tin(:,i) = parallelTemp(:,3);
+    Tmodel(1,:,1:N/2) = parallelTemp(1,3);
+    Tmodel(1,:,(N/2+1):N) = parallelTemp(1,2);
+end
+for i = 2:2:2*np-1
+    Tin(:,i) = parallelTemp(:,5);
+    Tmodel(1,:,1:N/2) = parallelTemp(1,5);
+    Tmodel(1,:,(N/2+1):N) = parallelTemp(1,4);
+end
+dTflow = zeros(1,2*np-1,N);
+dTplate = zeros(1,2*np-1,N); % Temperature change through interaction through plate
 for i = 1:tmax/dt-1
-    dTplate(1,1:N) = -(Tmodel(i,1,1:N)-Tmodel(i,2,1:N))/Rth*dt;
+    dTplate(1,1,1:N) = -(Tmodel(i,1,1:N)-Tmodel(i,2,1:N))/Rth*dt;
     for j = 2:2*np-2
         % First, convective heat transport through the fluid:
-        dTflow(j,1) = cflow*(Tin(i,j)-Tmodel(i,j,1));
-        dTflow(j,2:N) = cflow*(Tmodel(i,j,1:N-1)-Tmodel(i,j,2:N));
+        dTflow(1,j,1) = cflow*(Tin(i,j)-Tmodel(i,j,1));
+        dTflow(1,j,2:N) = cflow*(Tmodel(i,j,1:N-1)-Tmodel(i,j,2:N));
         % Second, through-plate heat transport
-        dTplate(j,1:N) = -(2*Tmodel(i,j,1:N)-Tmodel(i,j+1,1:N)-Tmodel(i,j-1,1:N))/Rth*dt;
+        dTplate(1,j,1:N) = -(2*Tmodel(i,j,1:N)-Tmodel(i,j+1,1:N)-Tmodel(i,j-1,1:N))/Rth*dt;
     end
-    dTplate(2*np-1,N) = -(Tmodel(i,j,N)-Tmodel(i,j-1,N))/Rth*dt/mWaterSegment/cp;
+    dTplate(1,2*np-1,N) = -(Tmodel(i,j,N)-Tmodel(i,j-1,N))/Rth*dt/mWaterSegment/cp;
     dT = dTflow + dTplate;
     Tmodel(i+1,:,:) = Tmodel(i,:,:) + dT;
 end
