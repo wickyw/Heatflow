d83201a9fa37c15a4f34dcd95d59e6cc4c27a541
mevrouwmispoes <renske1511@gmail.com>
2020-10-24 21:01:48 +0200

    hopelijk nu wel met plots

--- exchangerModelRenske.m
+++ exchangerModelRenske.m
@@ -12,10 +12,10 @@ df = 2.1*10^(-3); % Distance between panels
 l = 0.191; % length of panel, m
 w = 0.073; % width of panel, m
 np = 10; % number of plates
-N = 100; % number of model segments per plate
+N = 50; % number of model segments per plate
 dl = l/N; % length of model segment
 dt = 5; % time step, s
-tmax = 1000; % end time, s
+tmax = 5000; % end time, s
 v = 0; % Fluid flow rate, m/s
 labda = 401; %thermal heat conductivity of copper
 TempCooling = 20;
@@ -24,7 +24,7 @@ WaterDt = amountWater*dt;
 WaterRho = 0.997; %kg/L
 massWaterDt = WaterDt*WaterRho;
 heatCapWater = 4200;
-%% alleen warme stroom, plaat, koude stroom
+%% alleen warme stroom, plaat, koude stroom parallel
 koudeStroom = zeros(tmax/dt, N);
 koudeStroom(:,1) = TempCooling;
 warmeStroom = zeros(tmax/dt, N);
@@ -32,7 +32,7 @@ warmeStroom = zeros(tmax/dt, N);
 warmeStroom(1,:) = 60;
 koudeStroom(1,:) = TempCooling;
 tempPlaat = 17;
-AdeelPlaat = A/N;
+AdeelPlaat = Atot/N;
 Tnew= 60;
 for i = 2:tmax/dt
     warmeStroom(i,1) = Tnew;
@@ -44,26 +44,28 @@ for i = 2:tmax/dt
             a = labda*AdeelPlaat/dp;
 %             warmeStroom(i-1, j-1)
 %             koudeStroom(i-1, j-1)
-            Qwarm = -(labda*AdeelPlaat/dp)*dt*dl*(warmeStroom(i-1, j-1)-koudeStroom(i-1, j-1));
+            Qwarm = -2*(labda*AdeelPlaat/dp)*dt*10^(-3)*(warmeStroom(i, j-1)-koudeStroom(i, j-1));
 %             Qwarm/(massWaterDt*heatCapWater)
-            Qkoud = -Qwarm;
-            warmeStroom(i, j) = Qwarm/(massWaterDt*heatCapWater) + warmeStroom(i-1, j-1);
-            koudeStroom(i, j) = Qkoud/(massWaterDt*heatCapWater) + koudeStroom(i-1, j-1);
+            Qkoud = -2*(labda*AdeelPlaat/dp)*dt*10^(-3)*(koudeStroom(i, j-1)-warmeStroom(i, j-1));
+            warmeStroom(i, j) = Qwarm/(massWaterDt*heatCapWater) + warmeStroom(i, j-1);
+            koudeStroom(i, j) = Qkoud/(massWaterDt*heatCapWater) + koudeStroom(i, j-1);
 %         end
         
     end
-    Tnew = (Tnew*WaterRho*(10-WaterDt)+warmeStroom(i, N)*WaterRho*WaterDt)/(WaterRho*10)
+%     length_ = linspace(0, l, N);
+%     plot(warmeStroom(i, :));
+%     ylim([30 60]); 
+%     pause(0.1);   
+    Tnew = (Tnew*WaterRho*(10-WaterDt)+warmeStroom(i, N)*WaterRho*WaterDt)/(WaterRho*10);
 end
-%% Model data arrays initialisation:
-Tmodel = zeros(tmax/dt,np,N);
-for i = 1:tmax/dt
-    for j = 1:np
-        for k = 1:N
-            % First, convective heat transport through the fluid:
-            
-            
-            
-            
-        end
-    end
-end
+time = linspace(0, tmax, (tmax/dt));
+T1 = warmeStroom(:, 1);
+T2 = warmeStroom(:, end);
+T4 = koudeStroom(:, 2);
+T5 = koudeStroom(:, end);
+plot(time, T1);
+hold on;
+plot(time, T2);
+plot(time, T4);
+plot(time, T5);
+ylim([15 60]);
